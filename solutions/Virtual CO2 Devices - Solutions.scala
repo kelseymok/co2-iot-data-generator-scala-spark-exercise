2b37f67804f17f6710f600b43b83d714d7451ea8
Kelsey Mok <kelseymok@gmail.com>
2021-12-14 16:30:22 +0100

    km: adding new solutions and exercise

--- solutions/Virtual CO2 Devices - Solutions.scala
+++ solutions/Virtual CO2 Devices - Solutions.scala
@@ -76,6 +76,8 @@ def normalDistribution(mean: Double, stdDev: Double, x: Int): Double = {
 
 // COMMAND ----------
 
+// TEST MODULE. DO NOT MODIFY.
+
 import org.apache.commons.math3.stat.descriptive.SummaryStatistics
 
 def testNormalDistributionFunction(): Unit = {
@@ -124,7 +126,7 @@ exampleDf.withColumn("usingLiterals", col("interval") + lit(5)).show()
 // COMMAND ----------
 
 // MAGIC %md
-// MAGIC You might also try to use a custom Scala function, but it fails dramatically when trying to create a new column. Why does it fail?
+// MAGIC You might also try to use a custom Scala function, but it fails dramatically when trying to create a new column. **Why does it fail?**
 
 // COMMAND ----------
 
@@ -183,6 +185,7 @@ df.transform(createNewColumnWithFunction()).show()
 // COMMAND ----------
 
 // TESTING MODULE. DO NOT MODIFY.
+
 def testNormalDistributionFunction(): Unit = {
   val testDf = (1 to 10).toSeq.toDF("interval")
   
@@ -198,13 +201,14 @@ def testNormalDistributionFunction(): Unit = {
     (9, 0.0675486493639003), 
     (10, 0.04366227396405137)
   ).toDF("interval", "co2_ppm_ndist")
-  expected.show()
   
   val mean = 5.5
   val stdDev = 3.0276503540974917
   
   val result = testDf.transform(createNewColumnWithFunction())
   val diff = result.except(expected)
+  println("Diff between expected and result (should be 0 rows):")
+  diff.show()
   assert(diff.count() == 0)
   assert(result.columns.toSeq == Seq("interval", "co2_ppm_ndist"))
   println("All tests pass.")
@@ -248,7 +252,7 @@ def scaleDistBy(x: Int): DataFrame => DataFrame =
 
 df
   .transform(createNewColumnWithFunction())
-  .transform(scale_dist_by(5))
+  .transform(scaleDistBy(5))
   .show(10)
 
 
@@ -283,8 +287,9 @@ def testScaleBy(): Unit = {
   ).toDF("interval", "co2_ppm_ndist", "co2_ppm_ndist_x5")
   
   val result = testDf.transform(scaleDistBy(5))
-  result.show()
   val diff = result.except(expected)
+  println("Diff between expected and result (should be 0 rows):")
+  diff.show()
   assert(diff.count() == 0)
   assert(result.columns.toSeq == Seq("interval", "co2_ppm_ndist", "co2_ppm_ndist_x5"))
   println("All tests pass.")
@@ -349,8 +354,9 @@ def testScaleBy38(): Unit = {
   ).toDF("interval", "co2_ppm_ndist", "co2_ppm_ndist_x5", "co2_ppm_3_8")
   
   val result = testDf.transform(scaleBy38())
-  result.show()
   val diff = result.except(expected)
+  println("Diff between expected and result (should be 0 rows):")
+  diff.show()
   assert(diff.count() == 0)
   assert(result.columns.toSeq == Seq("interval", "co2_ppm_ndist", "co2_ppm_ndist_x5", "co2_ppm_3_8"))
   println("All tests pass.")
@@ -378,18 +384,20 @@ display(scaling38Df)
 
 // MAGIC %md
 // MAGIC ## Exercise: Adding some NOISE
-// MAGIC While we might be generating the same shape of data for every device, every device is different. In this exercise, we'll be adding some noise using the formula `randn(1)*10` in a new column called 'co2_ppm_noise'.
+// MAGIC While we might be generating the same shape of data for every device, every device is different. In this exercise, we'll be adding some noise using the formula `randn(seed)*10` in a new column called 'co2_ppm_noise'.
 // MAGIC 
 // MAGIC ### Resources
 // MAGIC * [Spark SQL Function randn](https://spark.apache.org/docs/latest/api/sql/index.html#randn)
 
 // COMMAND ----------
 
+import org.apache.spark.util.Utils
+
 /// YOUR CODE HERE
-def addNoise(): DataFrame => DataFrame =
+def addNoise(seed: Long = Utils.random.nextLong): DataFrame => DataFrame =
   df =>
     // Fill in ???. 
-    df.withColumn("co2_ppm_noise", col("co2_ppm_3_8") + randn(1)*10)
+    df.withColumn("co2_ppm_noise", col("co2_ppm_3_8") + randn(seed)*10)
 
 // COMMAND ----------
 
@@ -421,10 +429,10 @@ def testNoise(): Unit = {
     (10, 16591.66410633952, 16580.24487593498)
   ).toDF("interval", "co2_ppm_3_8", "co2_ppm_noise")
   
-  val result = testDf.transform(addNoise())
-  result.show()
-  
+  val result = testDf.transform(addNoise(1))
   val diff = result.except(expected)
+  println("Diff between expected and result (should be 0 rows):")
+  diff.show()
   assert(diff.count() == 0)
   assert(result.columns.toSeq == Seq("interval", "co2_ppm_3_8", "co2_ppm_noise"))
   println("All tests pass.")
@@ -485,18 +493,14 @@ df
 
 /// YOUR CODE HERE
 // Fill in the ???
+
+
 def cleanup(): DataFrame => DataFrame =
   df =>df
-    .transform(createNewColumnWithFunction())
-    .transform(scaleDistBy(5))
-    .transform(scaleBy38())
-    .transform(addNoise())
     .select(
       col("interval"),
       col("co2_ppm_noise").as("co2_ppm")
     )
-
-selectedDf.printSchema()
 
 // COMMAND ----------
 
@@ -530,7 +534,8 @@ def testCleanup(): Unit = {
   
   val result = testDf.transform(cleanup())
   val diff = result.except(expected)
-
+  println("Diff between expected and result (should be 0 rows):")
+  diff.show()
   assert(diff.count() == 0)
   assert(result.columns.toSeq == Seq("interval", "co2_ppm"))
   println("All tests pass.")
@@ -563,12 +568,18 @@ df
 
 /// YOUR CODE HERE
 // Replace ???
-def generateSingleSample(initialDf: DataFrame, deviceNumber: Int): DataFrame = {
+def generateSingleSample(initialDf: DataFrame, deviceNumber: Int, seed: Option[Long] = None): DataFrame = {
   initialDf
     .transform(createNewColumnWithFunction())
     .transform(scaleDistBy(5))
     .transform(scaleBy38())
-    .transform(addNoise())
+//     .transform(if (seed == None) addNoise() else addNoise(seed))
+    .transform(
+      seed match {
+        case Some(x) => addNoise(x)
+        case None => addNoise()
+      }
+    )
     .transform(cleanup())
     .withColumn("device", lit(deviceNumber))
 }
@@ -576,8 +587,8 @@ def generateSingleSample(initialDf: DataFrame, deviceNumber: Int): DataFrame = {
 val deviceNumbers = (1 to 10).toSeq
 
 // Replace ???
-def generateDataForAllDevices(initialDf: DataFrame, deviceNumbers: Seq[Int]): Seq[DataFrame] = {
-  deviceNumbers.map(x => generateSingleSample(initialDf, x)).toSeq
+def generateDataForAllDevices(initialDf: DataFrame, deviceNumbers: Seq[Int], seed: Option[Long] = None): Seq[DataFrame] = {
+  deviceNumbers.map(x => generateSingleSample(initialDf, x, seed)).toSeq
 }
 
 def combineIntoOneDataFrame(devices: Seq[DataFrame]): DataFrame = {
@@ -601,10 +612,12 @@ def testCleanup(): Unit = {
     (2,151610.3426226388,2),
     (3,91956.23595018136,2)
   ).toDF("interval", "co2_ppm", "device")
-  
-  val result = combineIntoOneDataFrame(generateDataForAllDevices(testDf, Seq(1, 2)))
+  val seed = Some(1.toLong)
+  val result = combineIntoOneDataFrame(generateDataForAllDevices(testDf, Seq(1, 2), seed))
   result.show()
   val diff = result.except(expected)
+  println("Diff between expected and result (should be 0 rows):")
+  diff.show()
   assert(diff.count() == 0)
   assert(result.columns.toSeq == Seq("interval", "co2_ppm", "device"))
   println("All tests pass.")
@@ -614,4 +627,272 @@ testCleanup()
 
 // COMMAND ----------
 
-display(combineIntoOneDataFrame(generateDataForAllDevices(df, deviceNumbers)))
+val transformedDf = combineIntoOneDataFrame(generateDataForAllDevices(df, deviceNumbers))
+display(transformedDf)
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Exercise: Calculate moving average
+// MAGIC Recall that we should notify users when their CO2 values exceed 1000ppm. But since we have quite some noise in our data, the values in reality might oscillate up and down around 1000ppm. We'd like to notify users if their CO2 values average to > 1000ppm in a 25 minute time span. Since each interval is 5 minutes, we're looking to calculate across 5 intervals (or 2 before and 2 after). Return the average in a column called `avg_window_co2_ppm`. Return the following dataframe columns (in this order): `device`, `interval`, `co2_ppm`, and `avg_window_co2_ppm`.
+// MAGIC 
+// MAGIC ### References
+// MAGIC * [Window functions](https://databricks.com/blog/2015/07/15/introducing-window-functions-in-spark-sql.html)
+// MAGIC * [.rowsBetween](https://spark.apache.org/docs/latest/api/python/reference/api/pyspark.sql.WindowSpec.rowsBetween.html)
+
+// COMMAND ----------
+
+import org.apache.spark.sql.functions.{avg, lag}
+import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.expressions.WindowSpec
+
+
+/// YOUR CODE HERE
+// Replace ???
+val co2PpmWindowSpec = Window.partitionBy("device").orderBy("interval").rowsBetween(-2, 2)
+
+def co2PpmMovingAverage(winSpec: WindowSpec): DataFrame => DataFrame =
+  df =>df
+    .select(
+      col("device"),
+      col("interval"),
+      col("co2_ppm"),
+      avg($"co2_ppm").over(winSpec) as "avg_window_co2_ppm")
+
+transformedDf.transform(co2PpmMovingAverage(co2PpmWindowSpec)).show()
+
+// COMMAND ----------
+
+// TESTING MODULE. DO NOT MODIFY.
+
+def testMovingAverageGenerator(device: Int): DataFrame => DataFrame =
+  df =>
+    df
+      .withColumn("device", lit(device))
+      .withColumnRenamed("co2_ppm", "co2_ppm_original")
+      .withColumn("co2_ppm", col("co2_ppm_original") + randn(device) * 10)
+      .drop("co2_ppm_original")
+    
+def testMovingAverage(): Unit = {
+    val tmpDf = Seq(
+    (1, 400.509717593963),
+    (2, 600.76282837649),
+    (3, 990.165608987576),
+    (4, 1200.72211550427),
+    (5, 1500.04597101324),
+    (6, 1500.297109300096),
+    (7, 1200.33939452365),
+    (8, 990.57160584409), 
+    (9, 600.13828771209), 
+    (10, 400.24487593498),
+  ).toDF("interval", "co2_ppm")
+  
+  val inputDf = tmpDf.transform(testMovingAverageGenerator(1))
+    .union(tmpDf.transform(testMovingAverageGenerator(2)))
+
+  val expected = Seq(
+  (1, 1, 417.3553288484079, 675.9734897379145),
+  (1, 2, 613.0388984708665, 808.2877105517177),
+  (1, 3, 997.5262418944693, 945.8408107506523),
+  (1, 4, 1205.2303729931275, 1161.280842604991),
+  (1, 5, 1496.0532115463907, 1275.5660491171955),
+  (1, 6, 1494.5554881201012, 1277.1284478598013),
+  (1, 7, 1184.464931031889, 1156.4403366895888),
+  (1, 8, 1005.3382356074973, 934.994823486399),
+  (1, 9, 601.789817142066, 795.1046573279735),
+  (1, 10, 388.8256455304417, 665.3178994266683),
+  (2, 1, 403.147400280263, 657.3686750394259),
+  (2, 2, 582.6365720920602, 785.507099491592),
+  (2, 3, 986.3220527459545, 930.1668278499885),
+  (2, 4, 1169.9223728480906, 1148.9706380461419),
+  (2, 5, 1508.8057412835744, 1269.7811799539938),
+  (2, 6, 1497.1664512610291, 1266.244293270555),
+  (2, 7, 1186.6892816313202, 1153.1113889815829),
+  (2, 8, 968.6376193287604, 928.137968239269),
+  (2, 9, 604.2578514032299, 785.8808474838289),
+  (2, 10, 383.93863757200506, 652.2780361013318)
+  ).toDF("device", "interval", "co2_ppm", "avg_window_co2_ppm")
+  val result = inputDf.transform(co2PpmMovingAverage(co2PpmWindowSpec))
+  val diff = result.except(expected)
+  println("Diff between expected and result (should be 0 rows):")
+  diff.show()
+  assert(diff.count() == 0)
+  assert(result.columns.toSeq == Seq("device", "interval", "co2_ppm", "avg_window_co2_ppm"))
+  println("All tests pass.")
+}
+
+testMovingAverage()
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC 
+// MAGIC ## Exercise: Calculating Slope of Values
+// MAGIC But wait! We only want to notify users when they have reached 1000pm on an upwards trend (not the downwards trend). Calculate the average slope of `co2_ppm` over the same moving averave window spec and add that to a column called `avg_slope`. Return the following columns: `interval`, `device`, `co2_ppm`, `avg_window_co2_ppm`, `avg_slope`.
+// MAGIC 
+// MAGIC Hint: to more easily calculate the slope between each interval, create a new column using the `lag` function. Then for each row, calculate the slope. Then return the columns that you need including the `avg_slope` which is calculated over the same Window Spec as `co2PpmMovingAverage` (previous exercise)
+// MAGIC 
+// MAGIC ### References
+// MAGIC * [Calculate Slope](https://en.wikipedia.org/wiki/Slope) - "rise over run"
+// MAGIC * [Lag Function](https://sparkbyexamples.com/spark/spark-sql-window-functions) - Section 3.2
+
+// COMMAND ----------
+
+import org.apache.spark.sql.functions.{avg, lag}
+
+/// YOUR CODE HERE
+// Replace ???
+def co2PpmSlopeMovingAverage(winSpec: WindowSpec): DataFrame => DataFrame =
+  df =>df
+    .withColumn("lag", lag("co2_ppm", 1).over(Window.partitionBy("device").orderBy("interval")))
+    .withColumn("slope", col("co2_ppm")-col("lag"))
+    .select(col("interval"), col("device"), col("co2_ppm"), col("avg_window_co2_ppm"), avg(col("slope")).over(winSpec) as "avg_slope")
+
+transformedDf
+  .transform(co2PpmMovingAverage(co2PpmWindowSpec))
+  .transform(co2PpmSlopeMovingAverage(co2PpmWindowSpec))
+  .show()
+
+
+// COMMAND ----------
+
+// TESTING MODULE. DO NOT MODIFY.
+    
+def testMovingAverageSlope(): Unit = {
+   
+  val inputDf = Seq(
+    (1, 1, 417.3553288484079, 675.9734897379145),
+    (1, 2, 613.0388984708665, 808.2877105517177),
+    (1, 3, 997.5262418944693, 945.8408107506523),
+    (1, 4, 1205.2303729931275, 1161.280842604991),
+    (1, 5, 1496.0532115463907, 1275.5660491171955),
+    (1, 6, 1494.5554881201012, 1277.1284478598013),
+    (1, 7, 1184.464931031889, 1156.4403366895888),
+    (1, 8, 1005.3382356074973, 934.994823486399),
+    (1, 9, 601.789817142066, 795.1046573279735),
+    (1, 10, 388.8256455304417, 665.3178994266683),
+    (2, 1, 403.147400280263, 657.3686750394259),
+    (2, 2, 582.6365720920602, 785.507099491592),
+    (2, 3, 986.3220527459545, 930.1668278499885),
+    (2, 4, 1169.9223728480906, 1148.9706380461419),
+    (2, 5, 1508.8057412835744, 1269.7811799539938),
+    (2, 6, 1497.1664512610291, 1266.244293270555),
+    (2, 7, 1186.6892816313202, 1153.1113889815829),
+    (2, 8, 968.6376193287604, 928.137968239269),
+    (2, 9, 604.2578514032299, 785.8808474838289),
+    (2, 10, 383.93863757200506, 652.2780361013318)
+  ).toDF("device", "interval", "co2_ppm", "avg_window_co2_ppm")
+  
+  val expected = Seq(
+    (1, 1,  417.3553288484079, 675.9734897379145, 290.0854565230307),
+    (2, 1,  613.0388984708665, 808.2877105517177, 262.6250147149065),
+    (3, 1,  997.5262418944693, 945.8408107506523, 269.6744706744957),
+    (4, 1, 1205.2303729931275, 1161.280842604991, 215.44003185433866),
+    (5, 1, 1496.0532115463907, 1275.5660491171955, 114.2852065122045),
+    (6, 1, 1494.5554881201012, 1277.1284478598013, 1.5623987426055919),
+    (7, 1,  1184.464931031889, 1156.4403366895888, -120.6881111702123),
+    (8, 1, 1005.3382356074973, 934.994823486399, -221.4455132031898),
+    (9, 1,   601.789817142066, 795.1046573279735, -276.43246064741487),
+    (10, 1,  388.8256455304417, 665.3178994266683, -265.21309516714905),
+    (1, 2,   403.147400280263, 657.3686750394259, 291.5873262328457),
+    (2, 2,  582.6365720920602, 785.507099491592, 255.5916575226092),
+    (3, 2,  986.3220527459545, 930.1668278499885, 276.4145852508278),
+    (4, 2, 1169.9223728480906, 1148.9706380461419, 218.8038101961532),
+    (5, 2, 1508.8057412835744, 1269.7811799539938, 120.81054190785201),
+    (6, 2, 1497.1664512610291, 1266.244293270555, -3.53688668343882),
+    (7, 2, 1186.6892816313202, 1153.1113889815829, -113.13290428897214),
+    (8, 2,  968.6376193287604, 928.137968239269, -224.97342074231386),
+    (9, 2,  604.2578514032299, 785.8808474838289, -278.306953422256),
+    (10, 2, 383.93863757200506, 652.2780361013318, -267.58354801977174)
+  ).toDF("interval", "device", "co2_ppm", "avg_window_co2_ppm", "avg_slope")
+    
+  val result = inputDf.transform(co2PpmSlopeMovingAverage(co2PpmWindowSpec))
+  val diff = result.except(expected)
+  println("Diff between expected and result (should be 0 rows):")
+  diff.show()
+  assert(diff.count() == 0)
+  assert(result.columns.toSeq == Seq("interval", "device", "co2_ppm", "avg_window_co2_ppm", "avg_slope"))
+  println("All tests pass.")
+}
+
+testMovingAverageSlope()
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Exercise: Return First Instance of Met Threshold
+// MAGIC Reminding ourselves that we need to alert our users if, during the the last 25 minutes, their CO2 levels (`avg_20m_co2_ppm`) are higher than 1000 and on the upward trend (positive `avg_slope`). Using only the Spark API, return only the FIRST instance in which this happens for each device. 
+// MAGIC 
+// MAGIC ### References
+// MAGIC * [Spark Where/Filter](https://sparkbyexamples.com/spark/spark-dataframe-where-filter/)
+// MAGIC * [RowNumber Function](https://sparkbyexamples.com/spark/spark-sql-add-row-number-dataframe)
+
+// COMMAND ----------
+
+import org.apache.spark.sql.functions.{row_number}
+
+/// YOUR CODE HERE
+// Replace ???
+def thresholdCrossed(): DataFrame => DataFrame =
+  df =>df
+    .filter(col("avg_slope") > 0 && col("avg_window_co2_ppm") > 1000)
+    .withColumn("row_index", row_number.over(Window.partitionBy("device").orderBy("interval")))
+    .where(col("row_index") === 1)
+    .drop("row_index")
+
+transformedDf
+  .transform(co2PpmMovingAverage(co2PpmWindowSpec))
+  .transform(co2PpmSlopeMovingAverage(co2PpmWindowSpec))
+  .transform(thresholdCrossed())
+  .show()
+
+// COMMAND ----------
+
+// TESTING MODULE. DO NOT MODIFY.
+    
+def testThresholdCrossed(): Unit = {
+   
+  val inputDf = Seq(
+    (1, 1,  417.3553288484079, 675.9734897379145, 290.0854565230307),
+    (2, 1,  613.0388984708665, 808.2877105517177, 262.6250147149065),
+    (3, 1,  997.5262418944693, 945.8408107506523, 269.6744706744957),
+    (4, 1, 1205.2303729931275, 1161.280842604991, 215.44003185433866),
+    (5, 1, 1496.0532115463907, 1275.5660491171955, 114.2852065122045),
+    (6, 1, 1494.5554881201012, 1277.1284478598013, 1.5623987426055919),
+    (7, 1,  1184.464931031889, 1156.4403366895888, -120.6881111702123),
+    (8, 1, 1005.3382356074973, 934.994823486399, -221.4455132031898),
+    (9, 1,   601.789817142066, 795.1046573279735, -276.43246064741487),
+    (10, 1,  388.8256455304417, 665.3178994266683, -265.21309516714905),
+    (1, 2,   403.147400280263, 657.3686750394259, 291.5873262328457),
+    (2, 2,  582.6365720920602, 785.507099491592, 255.5916575226092),
+    (3, 2,  986.3220527459545, 930.1668278499885, 276.4145852508278),
+    (4, 2, 1169.9223728480906, 1148.9706380461419, 218.8038101961532),
+    (5, 2, 1508.8057412835744, 1269.7811799539938, 120.81054190785201),
+    (6, 2, 1497.1664512610291, 1266.244293270555, -3.53688668343882),
+    (7, 2, 1186.6892816313202, 1153.1113889815829, -113.13290428897214),
+    (8, 2,  968.6376193287604, 928.137968239269, -224.97342074231386),
+    (9, 2,  604.2578514032299, 785.8808474838289, -278.306953422256),
+    (10, 2, 383.93863757200506, 652.2780361013318, -267.58354801977174)
+  ).toDF("interval", "device", "co2_ppm", "avg_window_co2_ppm", "avg_slope")
+  
+  val expected = Seq(
+    (4, 1, 1205.2303729931275,  1161.280842604991, 215.44003185433866),
+    (4, 2, 1169.9223728480906, 1148.9706380461419,  218.8038101961532)
+  ).toDF("interval", "device", "co2_ppm", "avg_window_co2_ppm", "avg_slope")
+  
+  val result = inputDf.transform(thresholdCrossed())
+  val diff = result.except(expected)
+  println("Diff between expected and result (should be 0 rows):")
+  diff.show()
+  assert(diff.count() == 0)
+  assert(result.columns.toSeq == Seq("interval", "device", "co2_ppm", "avg_window_co2_ppm", "avg_slope"))
+  println("All tests pass.")
+}
+
+testThresholdCrossed()
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ## Congratulations!
+// MAGIC Great job! You've written some code so that you'll be able to identify when a user's co2_ppm values breach the 1000ppm threshold. While you wait for the data to be streamed to you, a question to consider: how long should we buffer data until we calculate whether or not it's passed the 1000ppm threshold?
